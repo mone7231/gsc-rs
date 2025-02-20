2b4b88051c7864e3c3c9ecab6da5d84ba8014a69
mone7231 <mone.evilboy@gmail.com>
2025-02-20 15:11:22 +0800

    test2

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ const isProd = process.env.NODE_ENV === "production";
 const repoName = "gsc-rs"; // Replace with your GitHub repo name
 
 const nextConfig: NextConfig = {
+  output: "export", // Ensures static file generation
   assetPrefix: isProd ? `/${repoName}/` : "",
   basePath: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true, // Disable Next.js image optimization for GitHub Pages
   },
-  output: "export", // Ensures static file generation
+  
 };
 
 export default nextConfig;
